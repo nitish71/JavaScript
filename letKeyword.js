fc67df4af4b102f64a99ba951dca2e5870e7c0ea
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:09:32 +0530

    Create letKeyword.js

---
+++ letKeyword.js
@@ -0,0 +1,14 @@
+//variable declaration using Let keyword.
+let name = "Nitish Kumar";
+
+console.log(name);
+let anotherName = 'Sigma Kumar';
+console.log(anotherName);
+
+
+//const keyword example.
+const numba = 333;
+console.log(numba);
+
+const numberrr = 3333;
+console.log(numberrr);
