8a3b4bf6d106a673dd79a9ec6936a37a85f8e706
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:14:09 +0530

    Create stringInterpolations.js

---
+++ stringInterpolations.js
@@ -0,0 +1,18 @@
+//String Interploations
+
+console.log('We are learning string interpolation.');
+
+var name11 = 'Sales';
+var name12 = 'Force';
+var age11 = 23;
+
+console.log(name11+name12);
+var str = 'My name is '+ name11+name12+ ' and my age is '+age11;
+console.log(str);
+
+var num1111 = 1000;
+var num2222 = 2000;
+console.log(`The sum of number ${num1111} and ${num2222} is ${num1111+num2222}`);
+
+var recordID = '783rdyfe7f8efff';
+console.log(`https://salesforce.com/${recordID}`);
