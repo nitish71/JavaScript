6c4e7eccb818a98d38d290cb6004d45852d9cd83
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:14:32 +0530

    Create stringManipulation.js

---
+++ stringManipulation.js
@@ -0,0 +1,7 @@
+//string manipulations
+let str11 = 'Hi Guys, hope you are doing well';
+let check = str11.includes('you');
+console.log(check);
+console.log(str11.indexOf('well'));
+console.log(str11.endsWith('well'));
+console.log(str11.startsWith('Hi'));
