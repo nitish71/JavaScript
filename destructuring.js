46eb5f5abf98b8412394deeb34d72344bd274e19
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:13:40 +0530

    Create destructuring.js

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+//Destrcturing of array
+console.log('This is array desctructuring ');
+let arr21 = ['Amazon', 'Google', null];
+let [company1, company2, company3] = arr21;
+console.log(company1);
+console.log(company2);
+console.log(company3);
+
+//Object desctructuring
+console.log('This is object desctructuring ');
+let options= {
+        Fame: 'Nitish',
+    	age: 20,
+    	location2: 'Delhi'
+}
