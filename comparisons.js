5c2b95fc2165a851cef113cecea7523edfef4f9f
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:10:30 +0530

    Create comparisons.js

---
+++ comparisons.js
@@ -0,0 +1,18 @@
+/comparisons
+
+//Normal compariosns
+//strict comparisons
+//null comparisons
+console.log(3==3);
+console.log(3=='3');
+console.log('3'=='3');
+console.log(3===3);
+console.log(3 === '3');
+console.log('3' ===3);
+console.log('3'==='3');
+
+
+console.log('we are trying to understand undefiend variable here.');
+var x = null;
+console.log(x);
+console.log(typeof(x));
