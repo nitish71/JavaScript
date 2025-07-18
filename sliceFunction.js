c178874f35df02daee37f87229388cbd7397e472
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:15:02 +0530

    Create sliceFunction.js

---
+++ sliceFunction.js
@@ -0,0 +1,22 @@
+//slice function
+let newStr11 = str11.slice(0,11);
+console.log(newStr11);
+
+//lower and upper case
+console.log(newStr11.toLowerCase());
+console.log(newStr11.toUpperCase());
+
+//trim functions.
+var anotherSring = 'This          is Salesforce.com';
+console.log(anotherSring);
+console.log(anotherSring.trim());
+
+//object keys and values
+
+let obj11 = {
+    nname:'salesforce',
+    age: 23,
+    location: 'Delhi',
+    dob: '12/12/1999'
+
+}
