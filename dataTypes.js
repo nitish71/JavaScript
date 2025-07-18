e3e9b3e766a2af9d323222b4779c1f508818b938
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:10:03 +0530

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,34 @@
+//Data types in JS
+
+//number
+let num = 100;
+console.log(typeof(num));
+
+//string
+var firstName = 'Nitish';
+console.log(typeof(firstName));
+
+
+//boolean
+console.log(typeof(true));
+
+
+//big number
+var bigNum = 21887412874891;
+console.log(typeof(bigNum));
+
+//object data type
+var obj = {};
+console.log(typeof(obj));
+
+//funstion data type
+var sym = Symbol;
+console.log(typeof(sym));
+
+
+console.log('Now we are testing Null object.');
+//null data type.
+var x1 = 10;
+var x1 = null;
+console.log(x1);
+console.log(typeof(x1));
