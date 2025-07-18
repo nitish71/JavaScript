640dc5fc681a78da46b36e3d59088fa8b29853f6
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:08:56 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,13 @@
+//variable declaration using var keyword.
+var course = 'Zero to hero';
+console.log(course);
+
+course = 'Zero to hero in lightning';
+console.log('updated course is ' +course);
+
+console.log(window);
+
+function coursesss(){
+    var anotherCourse = "Expert in LWC";
+    console.log(anotherCourse);
+}
