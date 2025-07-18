b384698ad860c38af5c480c5d2497386a6950b33
Nitish Kumar <nitishgupta784@gmail.com>
2025-07-18 20:16:28 +0530

    Create exportFunction.js

---
+++ exportFunction.js
@@ -0,0 +1,11 @@
+//expoert in jaavascript
+
+import {PI2, add} from './utils.js'
+console.log(PI2)
+console.log(add(22,23));
+
+
+//Event handlers in javascript.
+function myFunctions(){
+    console.log('Hey Fam, how yall doing');
+}
